d62529a85398ad53353324dfd6f2a45ebc06b90f
Prian Kuhanandan <priankuhan@gmail.com>
2017-06-17 19:37:22 +0000

    postgres schema and user serializer bugfix

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170603173801) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "columns", force: :cascade do |t|
     t.string "name"
     t.string "data_type"
@@ -21,16 +24,16 @@ ActiveRecord::Schema.define(version: 20170603173801) do
   end
 
   create_table "columns_logbooks", id: false, force: :cascade do |t|
-    t.integer "logbook_id"
-    t.integer "column_id"
+    t.bigint "logbook_id"
+    t.bigint "column_id"
     t.index ["column_id"], name: "index_columns_logbooks_on_column_id"
     t.index ["logbook_id", "column_id"], name: "index_columns_logbooks_on_logbook_id_and_column_id", unique: true
     t.index ["logbook_id"], name: "index_columns_logbooks_on_logbook_id"
   end
 
   create_table "data_entries", force: :cascade do |t|
-    t.integer "entry_id"
-    t.integer "column_id"
+    t.bigint "entry_id"
+    t.bigint "column_id"
     t.string "data"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -39,7 +42,7 @@ ActiveRecord::Schema.define(version: 20170603173801) do
   end
 
   create_table "entries", force: :cascade do |t|
-    t.integer "logbook_id"
+    t.bigint "logbook_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["logbook_id"], name: "index_entries_on_logbook_id"
@@ -49,7 +52,7 @@ ActiveRecord::Schema.define(version: 20170603173801) do
     t.string "name"
     t.text "description"
     t.boolean "private", default: false
-    t.integer "user_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_logbooks_on_user_id"
@@ -66,4 +69,8 @@ ActiveRecord::Schema.define(version: 20170603173801) do
     t.index ["token"], name: "index_users_on_token", unique: true
   end
 
+  add_foreign_key "data_entries", "columns"
+  add_foreign_key "data_entries", "entries"
+  add_foreign_key "entries", "logbooks"
+  add_foreign_key "logbooks", "users"
 end
